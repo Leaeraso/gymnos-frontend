413c7ac166cdf635fa470ec8fcd41c0f9c6a5f5e
Facu Lopez <facundolopez026@gmail.com>
2025-05-17 18:49:58 -0300

    finish-style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,28 @@
-import { LoginForm } from "@/components/login-form";
-
+import { LoginForm } from "@/components/login-form"
+import Image from "next/image"
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            Horus Tech
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xl">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <Image
+          src="/login.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          layout="fill"
+        />
       </div>
     </div>
-  );
+  )
 }
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,61 +1,38 @@
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 export function LoginForm({
   className,
   ...props
-}: React.ComponentProps<"div">) {
+}: React.ComponentPropsWithoutRef<"form">) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Username</Label>
-                <Input
-                  id="username"
-                  type="text"
-                  placeholder="johndoe123"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full text-black">
-                  Login
-                </Button>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  );
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center text-center mb-4">
+        <h1 className="text-4xl font-bold">Bienvenido de nuevo a Gymnos</h1>
+      </div>
+      <div className="grid gap-6 w-90 mx-auto">
+        <div className="grid gap-2">
+          <Label htmlFor="username" className="text-xl">Usuario</Label>
+          <Input id="username" type="text" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password" className="text-xl">Contraseña</Label>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <Button type="submit" className="w-40 text-lg h-12 text-black">
+          Iniciar Sesion
+        </Button>
+      </div>
+    </form>
+  )
 }
